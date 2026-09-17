9203c3da9b7d52b4cfd7413d0087bdd0bdef05a8
joao gabriel <joao.gabriel.msilva10@gmail.com>
2026-09-16 22:13:37 -0300

    atualizacao de error

--- index.js
+++ index.js
@@ -13,20 +13,6 @@ app.use(express.json())
     // let expmloAlterar = 0, exemploAlterar = "batata"
 app.use(router)
 
-// Exercicio 15
-app.post("/api/maior", (req, res) => {
-  const num1 = req.body.num1  
-  const num2 = req.body.num2
-
-  if (num1 > num2) {
-    resultado = num1
-  } else if (num2 > num1) {
-    resultado = num2
-  }
-
-  res.send({ mensagem: resultado })
-})
-
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000')
 })
